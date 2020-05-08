c546ad2495ab507414ce1f2aff7c93ab52e572df
fitesa <marcosmunoz@fitesa.com.es>
2020-05-08 17:30:19 +0200

    first commit

---
+++ styles/IndicedeVegetacion_1_style.js
@@ -0,0 +1,61 @@
+var size = 0;
+var placement = 'point';
+
+var style_IndicedeVegetacion_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("NDVI");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value >= 0.158629 && value <= 0.299606) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(215,25,28,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.299606 && value <= 0.440583) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(253,174,97,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.440583 && value <= 0.581560) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(255,255,191,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.581560 && value <= 0.722537) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(171,221,164,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    } else if (value >= 0.722537 && value <= 0.863514) {
+            style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(43,131,186,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })]
+                    };
+
+    return style;
+};
